ab4dad98772e71ec6a3e509bfce5fea5b2e5da17
Przemyslaw <contact@pjsen.eu>
2023-03-13 13:31:23 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Running;
+using System;
+using System.Text.RegularExpressions;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+
+BenchmarkRunner.Run<Benchmark>(new FastConfig());
+
+[MemoryDiagnoser]
+public class Benchmark
+{
+    [GlobalSetup]
+    public void Setup()
+    {
+    }
+
+    [Benchmark(Baseline = true)]
+    public void Classes()
+    {
+        var a = new Item { Str = "aaa", Int = 21 };
+        var b = new Item { Str = "bbb", Int = 31 };
+
+        var result = a == b;
+    }
+
+    [Benchmark]
+    public void StructRecords()
+    {
+        var a = new SItem { Str = "aaa", Int = 21 };
+        var b = new SItem { Str = "bbb", Int = 31 };
+
+        var r = a.Equals(b);
+    }
+
+
+    class Item { public string Str { get; set; } public int Int { get; set; } }
+
+    struct SItem { public string Str { get; set; } public int Int { get; set; } }
+}
+
+public class FastConfig : ManualConfig
+{
+    public FastConfig()
+    {
+        Add(DefaultConfig.Instance);
+        AddJob(Job.Default
+            .WithLaunchCount(1)
+            .WithIterationTime(new(100, Perfolizer.Horology.TimeUnit.Millisecond))
+            .WithWarmupCount(3)
+            .WithIterationCount(3)
+            .WithPowerPlan(BenchmarkDotNet.Environments.PowerPlan.UserPowerPlan)
+        );
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+﻿- This is the template I use for benchmarking. I tend to open the folder with Visual Studio Code, no need for SLN file
+- I use always the newest BenchmarkDotNet thanks to `*` in package reference
+- I have some job settings applied which make the benchmark quicker. I also disabled the power profile switching, because I find it annoying on laptops
